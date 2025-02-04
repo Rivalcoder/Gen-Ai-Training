5bff8bb3e206fc4f27a95b06de214b0988e2d7fc
Rivalcoder <divi242006@gmail.com>
2025-02-04 15:17:17 +0530

    [Edit] Learn Objects

---
+++ Vercel-SDK/Generateobject/generateObject.js
@@ -0,0 +1,38 @@
+import { google } from '@ai-sdk/google';
+import { generateObject } from 'ai';
+import { z } from 'zod';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.local' });
+
+async function run() {
+  const apiKey = process.env.GOOGLE_GENERATIVE_AI_API_KEY;
+
+  try {
+
+    const { object } = await generateObject({
+        model:google('gemini-1.5-flash'),
+        schema: z.object
+                ({
+                    Topic: z.object({
+                    name: z.string().min(5).max(30),
+                    Topics: z.array(z.string()).min(10),
+                    Steps: z.array(z.string().min(100)).min(10),
+                 }),
+        }),
+        prompt:`Generate a structured response for FineTuning DeepSeek.
+                Ensure the following:
+                - Topic name is within 30 characters.
+                - Provide at least 10 sub-topics.
+                - Provide at least 10 steps in the learning process with min 100 words each.`,
+      });
+      
+      console.log(object);
+    }
+    catch (error) 
+    {
+    console.error('Error in streamText:', error);
+    }
+}
+
+run();
